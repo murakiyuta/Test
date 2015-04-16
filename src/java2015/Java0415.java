fb30cbc534a1c921f039ca03ab702d8df2846e83
x14g024 <x14g024@PC505N023.chiba-fjb.ac.jp>
2015-04-16 15:01:17 +0900

    テスト

---
+++ src/java2015/Java0415.java
@@ -0,0 +1,12 @@
+package java2015;
+
+public class Java0415 {
+
+	public static void main(String[] args) {
+		int i;
+		//iを0に初期設定;i<3を満たしていたら処理を実行;処理終了後、iに1を足す
+		for(i=0;i<3;i=i+1)  
+		    System.out.println(i);  //iの値を出力し改行
+	}
+
+}
